3cf8f96ab8933945037db3b982d8f3c2099a12d3
Jay Prakash Kumar <jp.kumar1508@gmail.com>
2020-10-22 01:17:37 +0530

    Collect & Exception

--- PracticeExampleOfRD/src/main/java/com/coforge/jay/Test.java
+++ PracticeExampleOfRD/src/main/java/com/coforge/jay/Test.java
@@ -1,5 +1,9 @@
 package com.coforge.jay;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
 
 	public static class Parent{
@@ -17,5 +21,12 @@ public class Test {
 	public static void main(String[] args) {
 		Parent p =new Child();
 		System.out.println(p.val + p.getVal());
+		
+		ArrayList al=new ArrayList();
+		al.add("ad");
+		al.add(2);
+		Map<String,String> map=new HashMap<>();
+		map.put("1", "2");
+		map.get("1");
 	}
 }
